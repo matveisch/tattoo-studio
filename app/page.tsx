6969c85a5b2026c904f25ef46960f24b6a2c643d
Matvei <matvesch@protonmail.com>
2024-12-31 11:38:04 +0200

    Update dependencies and enhance Home page layout
    - Added @radix-ui/react-dialog dependency to package.json and package-lock.json.
    - Introduced StudioPortfolio component in the Home page for improved content display.
    - Updated package-lock.json with new dependencies and their respective versions.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { ArtistsShowcase } from '../components/ArtistShowcase';
 import Hero from '../components/Hero';
-
+import { StudioPortfolio } from '../components/StudioPortfolio';
 export default function Home() {
   return (
     <div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Hero />
         <ArtistsShowcase />
+        <StudioPortfolio />
       </main>
     </div>
   );
