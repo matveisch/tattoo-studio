62fa19a57fd35fd60d2e645a1ad999c425dbdbd5
Matvei <matvesch@protonmail.com>
2025-01-12 14:15:25 +0200

    Refactor Supabase middleware for session management
    - Replaced the createClient function with an updateSession function to handle user session validation.
    - Implemented user redirection to the signin page for unauthorized access to admin routes.
    - Added detailed comments to guide proper usage of the Supabase client and response handling, ensuring session consistency and preventing premature logouts.

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,15 +1,12 @@
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export const createClient = (request: NextRequest) => {
-  // Create an unmodified response
+export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
+    request,
   });
 
-  createServerClient(
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
@@ -30,5 +27,41 @@ export const createClient = (request: NextRequest) => {
     }
   );
 
+  // Do not run code between createServerClient and
+  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+  // issues with users being randomly logged out.
+
+  // IMPORTANT: DO NOT REMOVE auth.getUser()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  console.log(user);
+
+  if (
+    !user &&
+    request.nextUrl.pathname.startsWith('/admin') &&
+    !request.nextUrl.pathname.includes('/signin')
+  ) {
+    // Only redirect if we're not already on the signin page
+    const url = request.nextUrl.clone();
+    url.pathname = '/admin/signin';
+    return NextResponse.redirect(url);
+  }
+
+  // IMPORTANT: You *must* return the supabaseResponse object as it is.
+  // If you're creating a new response object with NextResponse.next() make sure to:
+  // 1. Pass the request in it, like so:
+  //    const myNewResponse = NextResponse.next({ request })
+  // 2. Copy over the cookies, like so:
+  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+  // 3. Change the myNewResponse object to fit your needs, but avoid changing
+  //    the cookies!
+  // 4. Finally:
+  //    return myNewResponse
+  // If this is not done, you may be causing the browser and server to go out
+  // of sync and terminate the user's session prematurely!
+
   return supabaseResponse;
-};
+}
